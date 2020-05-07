286353eefb7b73c6f58ec0ffe47d4e156cfcafba
Richard Hyams <Rich2009@protonmail.ch>
2020-05-07 16:01:28 +0200

    post in form activated to update adverts in db

--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 from HadesV2App.Models import Advert
 
@@ -18,18 +18,48 @@ def home():
 
 
 @app.route("/getadverts")
-def getadverts():
+#def getadverts():
     
-    country='Mexico'
-    adverts = Advert.query.filter_by(country=country).all()
+    #country='Mexico'
+    #adverts = Advert.query.filter_by(country=country).all()
     
-    return render_template(
-        "adverts.html",
-        adverts=adverts,
-        country=country
-        
+    #return render_template(
+     #   "adverts.html",
+      #  adverts=adverts,
+       # country=country
+
+    
+
+    
+
+@app.route("/getadverts/<country>",methods=['GET','POST'])
+def getadverts(country):
+   
+    if request.method == 'GET':
+        adverts = Advert.query.filter_by(country=country).all()
+    
+        return render_template(
+            "adverts.html",
+            adverts=adverts,
+            country=country
 
     )
+
+    if request.method == 'POST':
+        advert_id = request.form.get('advert_id')
+        category = request.form.get('category')
+        business = request.form.get('business')
+
+
+        print(advert_id,category,business)
+        adverts = Advert.query.filter_by(country=country).all()
+        return render_template(
+            "adverts.html",
+            adverts=adverts,
+            country=country
+
+    )
+
 
 @app.route("/contact")   
 def contact():
