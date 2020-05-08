6104135740e82484dffc7b1bc59da057c01a5bbe
Richard Hyams <Rich2009@protonmail.ch>
2020-05-08 18:28:24 +0200

    o model and use in home

--- Models.py
+++ Models.py
@@ -3,5 +3,7 @@ from HadesV2App import db
 class Advert(db.Model):
     __tablename__ = 'advert'
     __table_args__ = { 'extend_existing': True }
-    advert_id = db.Column(db.Integer, primary_key=True)   
+    advert_id = db.Column(db.Integer, primary_key=True) 
+    country= db.Column(db.Text)
+
 
