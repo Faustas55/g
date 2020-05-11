4a8e41cdd05161dbf64c30b3104c20ea62e32eec
Richard Hyams <Rich2009@protonmail.ch>
2020-05-11 10:07:36 +0200

    added updated fields

--- db/import_data_db.py
+++ db/import_data_db.py
@@ -9,6 +9,7 @@
 
 import pandas as pd
 import numpy as np
+import datetime
 
 import_file='c:\sqlite\db\online.csv'
 export_file='c:\sqlite\db\online_transformed.csv'
@@ -16,7 +17,7 @@ export_file='c:\sqlite\db\online_transformed.csv'
 
 export_cols=['advert_id','region', 'country', 'product', 'price', 'cur', 'seller',
        'category', 'last_seen', 'type', 'domain', 'url',
-       'date_found', 'business', 'product_brand','polonius_caseid']
+       'date_found', 'business', 'product_brand','polonius_caseid','updated_date','updated_by']
 
 rename_cols={'Product_category_for_polonius':'product_brand','Business':'business',
                     'report_date':'date_found','cat':'type'
@@ -34,8 +35,11 @@ df.drop(['score','set_category'], axis=1,inplace=True)
 #this is for SQLite rowid , it will just update the row id automatically 
 df['advert_id']='NULL'
 
+
 #make polonius_caseid null
 df['polonius_caseid']='NULL'
+df['updated_by']='upload'
+df['updated_date']=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 df=df.rename(columns=rename_cols)
 
                 
