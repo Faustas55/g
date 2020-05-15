9d27bd04eed99d11e92237e3dc9d7bd8391b8ba8
Richard Hyams <Rich2009@protonmail.ch>
2020-05-15 12:02:36 +0200

    updated comments

--- db/import_data_db.py
+++ db/import_data_db.py
@@ -1,19 +1,22 @@
 #script to import data from the splunk online report 
 # into hades db on sqlite 
 
-#TODO remeber when importing data make sure to use UTF-8 specified and not system
-#TODO run an Sqlite backup before import
 
-#TODO add in a region if none 
+#TODO Sqlite admin : run an Sqlite backup before import ..schedule on server or create a script ? 
+
+#TODO add in a region if none - Faustas 
 
 
 import pandas as pd
 import numpy as np
 import datetime
+
 from sqlalchemy.types import Integer,Text
 from sqlalchemy import create_engine
 engine = create_engine('sqlite:///HadesV2App/db/hades.db', echo=True)
 
+
+#getting the advert table and convert into a dataframe ... 
 df_db=pd.read_sql('SELECT * FROM advert', engine)
 
 
@@ -23,58 +26,71 @@ import_file='c:\sqlite\db\online.csv'
 #We can continue with this intially if we have problems importing with pandas we can use the GUI to import
 export_file='c:\sqlite\db\online_transformed.csv'
 
-
+#These are the columns only needed for the export to csv file 
 export_cols=['advert_id','region', 'country', 'product', 'price', 'cur', 'seller',
        'category', 'last_seen', 'cat', 'domain', 'url',
        'date_found', 'business', 'product_brand','polonius_caseid','updated_date','updated_by']
 
+
+#we need to rename the columns as they are different in the splunk report , plus whilst
+#merging new columns are created 
 rename_cols={'Product_category_for_polonius':'product_brand','Business':'business',
                     'report_date':'date_found','cat_y':'cat','region_y':'region', 'country_y':'country', 
                     'price_y':'price','cur_y':'cur', 'category_y':'category', 'last_seen_y':'last_seen', 'url_y':'url'
             }
 
+#Lets drop all the useless columns from the merge and the splunk report 
 columns_drop=['advert_id', 'region_x', 'country_x', 'price_x', 'cur_x',
                'category_x', 'last_seen_x', 'cat_x', 'url_x',
               'date_found', 'business', 'product_brand', 'polonius_caseid',
                      'updated_date', 'updated_by','_merge']
 
 
-#read in the csv
+#read in the csv from splunk
 df=pd.read_csv(import_file)
 print(df.head())
 
 
-#drop the useless columns 
+#drop some more useless columns before we merge 
 df.drop(['score','set_category'], axis=1,inplace=True)
 
 #merge so we only get new adverts .New adverts=new seller + domain + product
 df_merge=df_db.merge(df,indicator=True,how='outer',on=['seller','product','domain'])
-print(df_db.shape)
-print(df.shape)
-print()
-print(df_merge[df_merge['_merge']=='right_only'].shape)
+
+#debugging stuff not needed and will probably delete
+#print(df_db.shape)
+#print(df.shape)
+
+#print(df_merge[df_merge['_merge']=='right_only'].shape)
 #print (df_merge['_merge'].value_counts())
 #print (df_merge.columns)
 
-#take only the right sided ones ..i.e the new ones which have got from the merge
+#take only the right sided ones . these are adverts that are not in the sqlite database ..i.e the new ones
 df=df_merge[df_merge['_merge']=='right_only']
+
+#drop all the useless columns created by the merge .. i.e the adverts already in database
 df.drop(columns_drop,axis=1,inplace=True)
 
 
-#this is for SQLite rowid , it will just update the row id automatically 
+#this is for SQLite rowid which is a primary key, send in a null object and sqlite will sort this out
 df.loc[:,'advert_id']=None
 
 
 #make polonius_caseid null
 df.loc[:,'polonius_caseid']=None
 
+#set that the "upload user " has updated 
 df.loc[:,'updated_by']='upload'
+#and when uploaded
 df.loc[:,'updated_date']=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+#rename the columns so it fits the table columns
 df=df.rename(columns=rename_cols)
-print(df.columns)
+
                 
-#write out the csv to be uploaded
+#write out the csv to be uploaded ..this is now just a backup 
 df.to_csv(export_file,index=False, columns=export_cols, )
 
+#write the adverts back to the table "advert" as one big hit 
 df.to_sql('advert', con=engine, if_exists='append',
           index=False,dtype={"business": Text(),"product_brand":Text()})
