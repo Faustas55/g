7004351ba372a21b3cd03c5cd7cd369081fcb365
Faustas55 <65541015+Faustas55@users.noreply.github.com>
2020-05-25 10:11:10 +0200

    Update import_data_db.py

--- db/import_data_db.py
+++ db/import_data_db.py
@@ -28,7 +28,7 @@ df_db=pd.read_sql('SELECT * FROM advert', engine)
 import_file='c:\sqlite\db\online.csv'
 
 #importing a file with countries and regions that will be joined with the main database - F
-df_region='c:\sqlite\db\regioncountry.csv'
+df_region=('c:\sqlite\db\regioncountry.csv', keep_default_na=False, na_values=['_'])
 
 #We can continue with this intially if we have problems importing with pandas we can use the GUI to import
 export_file='c:\sqlite\db\online_transformed.csv'
