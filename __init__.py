ea1c6b45b69af0ab868a5d43647dd5057b7ef62b
Richard Hyams <Rich2009@protonmail.ch>
2020-06-06 17:28:26 +0200

    duplicate crap

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app = Flask(__name__)
+
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/hades.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
