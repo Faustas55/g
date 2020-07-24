9ef412702523979b949fc6e6c1d9a4b6e856f92f
Faustas55 <65541015+Faustas55@users.noreply.github.com>
2020-07-24 18:24:17 +0200

    added no action all

--- db/import_data_db.py
+++ db/import_data_db.py
@@ -271,8 +271,10 @@ if not df.empty:
     df = df.rename(columns=rename_cols)
     # merging the seller statistics with the main df
     df= pd.merge(df, df_categories, on=['seller'], how="left")
-    df.rename(columns={'no_action_y':'no_action', 'suspected_counterfeiter_y':'suspected_counterfeiter', 'takedown_y':'takedown'}, inplace=True) 
-    df.drop(columns=['no_action_x', 'takedown_x', 'suspected_counterfeiter_x', 'set_category'], axis=1, inplace=True)
+    df.rename(columns={'no_action_y':'no_action', 'suspected_counterfeiter_y':'suspected_counterfeiter', 'takedown_y':'takedown'}, inplace=True)
+    #adds no action all and no action together
+    df['no_action'] = df['no_action'] + df['no_action_all']
+    df.drop(columns=['no_action_x', 'takedown_x', 'suspected_counterfeiter_x', 'set_category', 'no_action_all'], axis=1, inplace=True)
     df.drop_duplicates(subset=['seller','domain'],inplace=True)
     # fillna so we get a numerical value instead of none
     df[['no_action', 'suspected_counterfeiter','takedown']]=df[['no_action', 'suspected_counterfeiter','takedown']].fillna(value=0)
