dd2adda134e111b8f9432bf89fc0312d2108b4bb
Faustas55 <65541015+Faustas55@users.noreply.github.com>
2020-07-29 12:05:19 +0200

    added archived

--- statistics.py
+++ statistics.py
@@ -17,7 +17,7 @@ df_db.to_csv(r"C:\Splunk\intel\hadesresults\hadesresults.csv")
 #grouping by and cleaning the data
 df_groupedby=df_db.groupby(['region','category']).size().unstack(level=1, fill_value=0).reset_index()
 df_groupedby['authorised']=df_groupedby['authorised']+df_groupedby['syngenta authorised']
-df_groupedby.drop(columns={'uncategorised\nuncategorised', 'syngenta authorised'}, axis=1, inplace=True)
+df_groupedby.drop(columns={'uncategorised\nuncategorised', 'syngenta authorised', 'archived'}, axis=1, inplace=True)
 
 #adding the data so it's possible to timechart it
 df_groupedby.loc[:, "uploaded_date"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
