de0cd5726365b2a834e5667d3ccb795c6abb37d8
Faustas55 <65541015+Faustas55@users.noreply.github.com>
2020-10-26 11:19:53 +0100

    MYSQL rewrite

--- __init__.py
+++ __init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from HadesV2App import config
+import pymysql
+pymysql.install_as_MySQLdb()
+import MySQLdb
+
 
 app = Flask(__name__)
 
 
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/hades.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://'+ config.username +":" + config.password +"@localhost/"+ config.database
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 
