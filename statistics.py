3374f6354116e3d6c7cb0f21f528065d1686849e
Faustas55 <65541015+Faustas55@users.noreply.github.com>
2020-11-13 16:08:36 +0100

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -24,10 +24,10 @@ df_db.to_csv(r"C:\Splunk\intel\hadesresults\hadesresults.csv")
 #grouping by and cleaning the data
 df_groupedby=df_db.groupby(['region','category']).size().unstack(level=1, fill_value=0).reset_index()
 df_groupedby['authorised']=df_groupedby['authorised']+df_groupedby['syngenta authorised']
-df_groupedby.drop(columns={'uncategorised\nuncategorised', 'syngenta authorised', 'archived', 'no action all'}, axis=1, inplace=True)
+df_groupedby.drop(columns={'uncategorised\nuncategorised', 'syngenta authorised', 'archived', 'no action all', 'Outofstock/Paused'}, axis=1, inplace=True)
 
 #adding the data so it's possible to timechart it
 df_groupedby.loc[:, "uploaded_date"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 #export
-df_groupedby.to_csv(r"C:\Splunk\intel\hadesresults\hadesstatistics.csv", mode='a', header=False, index=False)
+df_groupedby.to_csv(r"C:\Splunk\intel\hadesresults\hadesstatistics.csv", mode='a', header=True, index=False)
