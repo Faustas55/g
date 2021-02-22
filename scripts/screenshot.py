d048c68997017a5d9d2ec921bac86d02a8294078
Richard Hyams <Rich2009@protonmail.ch>
2021-02-22 13:50:11 +0100

    working version

--- scripts/screenshot.py
+++ scripts/screenshot.py
@@ -1,6 +1,39 @@
 from selenium import webdriver
 from time import sleep
 from mysql_utils import get_adverts
+import os
+
+from random import randint
+
+
+def lst_find_dupl(list1,list2):
+    """
+    function to find the duplicates in the list and return the duplicates 
+    or non duplicates matches between the lists
+
+    Args:
+        list(strings(list1)): list of strings to compare 
+        list(strings(list2)): list of strings to compare
+        
+
+    Returns:
+        list(items_dup) : items that match between two lists
+        list(items_nondup) : items that do not match between two lists
+
+    """
+    items_dup=[]
+    items_nondup=[]
+    for item in list1:
+            if item in list2:
+                items_dup.append(item)
+            else: 
+                items_nondup.append(item)
+    
+    return items_dup,items_nondup        
+
+
+    
+
 
 #variables to select the relevant adverts 
 
@@ -8,11 +41,14 @@ site='%mercadolibre%'
 review='Sent to CSC for Takedown'
 fromdate ='2021-01-01'
 
-#set up directory
-dir='c:\\temp\\screenshots'
+#set up directory to add in screenshots 
+dir='c:\\temp'
+path=os.path.join(dir,'screenshots')
 
+#keep a list of already done screenshots to stop repeating 
+files=[]
 
-#set up headless chrome browser 
+#set up headless chrome browser to render the pages before screenshots
 options = webdriver.ChromeOptions() 
 options.add_argument("headless")
 driver = webdriver.Chrome(chrome_options=options)
@@ -20,22 +56,35 @@ driver = webdriver.Chrome(chrome_options=options)
 
 
 #get mercadolibre list from database = takedowns
-adverts=get_adverts(f"select url,advert_id,updated_date,category from advert where domain like '{site}' and review='{review}' and updated_date >= '{fromdate}' ")
+adverts=get_adverts(f"select url,advert_id from advert where domain like '{site}' and review='{review}' and updated_date >= '{fromdate}' ")
 
+#has it already been screenshotted in the past ?
+# get list of file names 
+with os.scandir(path) as listOfEntries:
+    for entry in listOfEntries:
+        # print all entries that are files
+        if entry.is_file():
+            files.append(entry.name.strip('.png'))
 
-#setup the unique name  
+#check files against advert ids 
 
+advert_ids=[str(dict['advert_id']) for dict in adverts]
+
+lst_dupl,lst_non_dupl=lst_find_dupl(advert_ids,files)  
 
 #for each mercadolibre link get a screenshot 
 
+for dict in adverts:
 
-driver.get('https://articulo.mercadolibre.com.mx/MLM-712227134-advion-mata-cucarachas-30g-efectivo-veneno-gel-jeringa-_JM')
-sleep(3)
+    url,advert_id = dict.values()
+    
+    if str(advert_id) in lst_non_dupl:
+        driver.get(url)
+        sleep(randint(2, 10))
 
-#S = lambda X: driver.execute_script('return document.body.parentNode.scroll'+X)
-driver.set_window_size(2000,5000) # May need manual adjustment                                                                                                                
-driver.find_element_by_tag_name('body').screenshot('c:\\temp\\screenshots\\web_screenshot.png')
-
-#update the database with nameofscreenshot 
+    #S = lambda X: driver.execute_script('return document.body.parentNode.scroll'+X)
+        driver.set_window_size(2000,5000) # May need manual adjustment                                                                                                                
+        driver.find_element_by_tag_name('body').screenshot(os.path.join(path,str(advert_id)+'.png'))
+        print(f'Advert {advert_id} screenshotted and saved as {str(advert_id)+".png"}')
 
 driver.quit()
