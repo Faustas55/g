d734043e1678310717fd19c5afeb30705f991e30
Faustas55 <65541015+Faustas55@users.noreply.github.com>
2021-02-26 16:12:08 +0100

    adding bootstrap

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 from HadesV2App import config
 import pymysql
 pymysql.install_as_MySQLdb()
@@ -8,14 +9,13 @@ import MySQLdb
 
 app = Flask(__name__)
 
-
 app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://'+ config.username +":" + config.password +"@localhost/"+ config.database
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 
 
 db = SQLAlchemy(app)
-
+bootstrap = Bootstrap(app)
 # This is the lazy mans version maybe I will add in the columns into the model
 db.Model.metadata.reflect(db.engine)
 
