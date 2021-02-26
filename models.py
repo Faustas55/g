530379936b64ba44d9657dcc934b5e4404ae545f
Faustas55 <65541015+Faustas55@users.noreply.github.com>
2021-02-26 16:11:25 +0100

    new mysql table

--- models.py
+++ models.py
@@ -12,3 +12,12 @@ class Advert(db.Model):
     updated_date = db.Column(db.Text)
     seller = db.Column(db.Text)
     polonius_caseid=db.Column(db.Integer)
+    
+class Takedown(db.Model):
+    __tablename__ = "takedowns"
+    __table_args__ = {"extend_existing": True}
+    advert_id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.Text)
+    domain = db.Column(db.Text)
+    takedown_confirmed = db.Column(db.Text)
+    review = db.Column(db.Text)
